da4142cf3af228f51a7ac390e532eebee58bbcbb
Gabriel <gpsnts@protonmail.com>
2021-03-06 23:12:44 -0300

    WIP Wrap "init"

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+// #include <glad/glad.h>
+// #include <GLFW/glfw3.h>
+#include "headers/game.hpp"
 #include <iostream>
 
 using namespace std;
@@ -8,32 +9,16 @@ const char* APP_NAME = "OpenGL WIP Game";
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void close_window_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-	if (key  == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-}
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	Game::setup_opengl_glfw();
 
-	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, APP_NAME, NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, APP_NAME, NULL, NULL);
 
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
 	}
@@ -41,10 +26,10 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	// ! Set framebuffer size change adjust callback
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetFramebufferSizeCallback(window, Game::framebuffer_size_callback);
 	
 	// ! Set close window callback
-	glfwSetKeyCallback(window, close_window_callback);
+	glfwSetKeyCallback(window, Game::close_window_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -54,10 +39,13 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// ! FPS Info
+		Game::frames_per_second(window);
+		
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.23f, 0.01f, 0.45f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 	}
 
